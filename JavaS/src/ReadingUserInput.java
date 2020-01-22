deade076cb9cdeb2a3d0d6d53d45c21475530bd6
oliveira_lucas <lucas.oliveira@celfocus.com>
2020-01-22 23:40:41 +0000

    adding new features

--- JavaS/src/ReadingUserInput.java
+++ JavaS/src/ReadingUserInput.java
@@ -9,16 +9,34 @@ public class ReadingUserInput
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println("Enter your year of birth:");
-		int yearOfBirth = scanner.nextInt();
-		scanner.nextLine(); // handle next line character (enter key)
 
-		System.out.println("Enter your name: ");
-		String name = scanner.nextLine();
+		boolean hasNextInt = scanner.hasNextInt();
 
-		int age = Calendar.getInstance().get(Calendar.YEAR) - yearOfBirth;
+		if (hasNextInt)
+		{
+			int yearOfBirth = scanner.nextInt();
+			scanner.nextLine(); // handle next line character (enter key)
 
-		System.out.println("Your name is " + name);
-		System.out.println("And you are " + age + " years old.");
+			System.out.println("Enter your name: ");
+			String name = scanner.nextLine();
+
+			int age = Calendar.getInstance().get(Calendar.YEAR) - yearOfBirth;
+
+			if (age >= 0 && age <= 100)
+			{
+				System.out.println("Your name is " + name);
+				System.out.println("And you are " + age + " years old.");
+			}
+			else
+			{
+				System.out.println("Invalid year of birth");
+			}
+		}
+		else
+		{
+			System.out.println("Unable to parse year of birth.");
+		}
+
 		scanner.close();
 	}
 
