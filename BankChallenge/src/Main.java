df30cd128316aa68f829b611b250134c0e00b569
OliveiraLucas <oliveiraborgeslucas@gmail.com>
2020-01-24 22:59:12 +0000

    new basic classes

--- BankChallenge/src/Main.java
+++ BankChallenge/src/Main.java
@@ -4,15 +4,19 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		Account account1 = new Account(1, 100.00, "Lucas Oliveira", "oliveiralucas@gmail.com", "9304068017");
-		System.out.println(account1.toString());
-		account1.deposit(55.30);
-		System.out.println(account1.toString());
-		account1.withdraw(500);
-		System.out.println(account1.toString());
-		account1.withdraw(155.2);
-		System.out.println(account1.toString());
+		// Account account1 = new Account(1, 100.00, "Lucas Oliveira", "oliveiralucas@gmail.com", "9304068017");
+		// System.out.println(account1.toString());
+		// account1.deposit(55.30);
+		// System.out.println(account1.toString());
+		// account1.withdraw(500);
+		// System.out.println(account1.toString());
+		// account1.withdraw(155.2);
+		// System.out.println(account1.toString());
 
+		VipCustomer vip = new VipCustomer();
+		System.out.println(vip.toString());
+		VipCustomer vip2 = new VipCustomer("Lucas Oliveira", "myvipemail@google.com");
+		System.out.println(vip2);
 	}
 
 }
---
+++ BankChallenge/src/VipCustomer.java
@@ -0,0 +1,48 @@
+
+
+public class VipCustomer
+{
+	private String name;
+	private double creditLimit;
+	private String email;
+	
+	public VipCustomer() {
+		this("Vip Customer", 1000, "myvip@email.com");
+	}
+	
+	public VipCustomer(String name, String email) {
+		this(name, 350, email);
+	}
+	
+	public VipCustomer(String name, double creditLimit, String email) {
+		this.name = name;
+		this.creditLimit = creditLimit;
+		this.email = email;
+	}
+	
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public double getCreditLimit()
+	{
+		return creditLimit;
+	}
+	
+	public String getEmail()
+	{
+		return email;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "VipCustomer [name=" + name + ", creditLimit=" + creditLimit + ", email=" + email + "]";
+	}
+	
+	
+	
+	
+}
