6141bd74b0a3d7838b1784cf208d49f8b7a9eef3
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-02-10 22:40:51 +0000

    new features with sort

--- Java 8 Book/src/com/oliveiralucas/TestingRemoveIf.java
+++ Java 8 Book/src/com/oliveiralucas/TestingRemoveIf.java
@@ -1,14 +1,17 @@
 package com.oliveiralucas;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
 
 public class TestingRemoveIf
 {
 
 	public static void main(String... args)
 	{
-		List<Usuario> usuarios = new ArrayList<>(); // arraysList é uma colleção mutavel, aceita mudançã
+		List<Usuario> usuarios = new ArrayList<Usuario>(); // arraysList é uma colleção mutavel, aceita mudançã
 		usuarios.add(new Usuario("Lucas", 150));
 		usuarios.add(new Usuario("Pedro", 101));
 		usuarios.add(new Usuario("Maico", 50));
@@ -16,5 +19,16 @@ public class TestingRemoveIf
 
 		usuarios.removeIf(u -> u.getPontos() < 100);
 		usuarios.forEach(u -> System.out.println(u.getNome()));
+
+		ToIntFunction<Usuario> extraiPontos = u -> u.getPontos();
+		ToIntFunction<Usuario> extraiPontos2 = Usuario::getPontos;
+		Comparator<Usuario> comparator = Comparator.comparingInt(extraiPontos);
+		usuarios.sort(comparator);
+		usuarios.forEach(u -> System.out.println(u.getPontos()));
+		usuarios.sort(Comparator.comparingInt(Usuario::getPontos));
+		usuarios.forEach(u -> System.out.println(u.getPontos()));
+
+		Function<Usuario, String> byName = Usuario::getNome;
+		usuarios.sort(Comparator.comparing(byName));
 	}
 }
